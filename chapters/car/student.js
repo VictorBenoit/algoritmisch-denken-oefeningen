cc8469941441ae8472a604ff4fde7505588502f3
Victor Benoit <71572848+DivisionEscaper@users.noreply.github.com>
2020-09-28 11:53:37 +0200

    car

--- chapters/car/student.js
+++ chapters/car/student.js
@@ -1,1 +1,72 @@
 // Schrijf hier je code
+function myFirstFunction(bike) {
+    forward(bike);
+}
+function twiceForward(bike) {
+    forward(bike);
+    forward(bike);
+}
+function thriceForward(bike) {
+    forward(bike);
+    forward(bike);
+    forward(bike);
+}
+function forward4(bike) {
+    forward(bike);
+    forward(bike);
+    forward(bike);
+    forward(bike);
+}
+function forward5(bike) {
+    let i = 5;
+    while (i > 0) {
+        forward(bike);
+        i = i - 1;
+    }
+}
+function forward10(bike) {
+    let i = 10;
+
+    while (i > 0) {
+        forward(bike);
+        i = i - 1;
+    }
+}
+function right(bike) {
+    turnRight(bike);
+    forward(bike);
+}
+function ellShape(bike) {
+    forward5(bike);
+    turnRight(bike);
+    forward4(bike);
+}
+function uTurn(bike) {
+    thriceForward(bike);
+    turnRight(bike);
+    forward10(bike);
+    turnRight(bike);
+    twiceForward(bike);
+}
+function forwardN(bike, steps) {
+    let i = steps;
+
+    while (i > 0) {
+        forward(bike);
+        i = i - 1;
+    }
+}
+function crookedUTurn(bike) {
+    forwardN(bike, 7);
+    turnRight(bike);
+    forwardN(bike, 9);
+    turnRight(bike);
+    forwardN(bike, 3);
+
+}
+function forwardUntilWall(bike) {
+    while (!sensor(bike)) {
+        forward(bike);
+    }
+}
+
